bb1af15423d1a09868b789681f0955dba1138ed5
Agustin <agustin@agustin-t460.midokura.local>
2018-02-21 17:17:51 +0100

    tipo in folder name

--- scripts/getKernelSources.sh
+++ scripts/getKernelSources.sh
@@ -7,5 +7,5 @@ wget http://developer.download.nvidia.com/embedded/L4T/r28_Release_v1.0/BSP/sour
 tar -xvf source_release.tbz2 sources/kernel_src-tx2.tbz2
 tar -xvf sources/kernel_src-tx2.tbz2
 cd kernel/kernel-4.4
-patch -p0 < /home/nvidia/buildJetsonTx2Kernel/diffs/kernelconfig/config.patch
-patch -p0 < /home/nvidia/buildJetsonTx2Kernel/diffs/kernelconfig/makefile.patch
+patch -p0 < /home/nvidia/buildJetsonTX2Kernel/diffs/kernelconfig/config.patch
+patch -p0 < /home/nvidia/buildJetsonTX2Kernel/diffs/kernelconfig/makefile.patch
