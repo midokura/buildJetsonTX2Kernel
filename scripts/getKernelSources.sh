ab9069c7b0249601ca2016f99ac2fac929d2d4dd
Agustin Rivero <txtmode@gmail.com>
2019-05-16 13:38:21 +0200

    Kernel 4.9

--- scripts/getKernelSources.sh
+++ scripts/getKernelSources.sh
@@ -3,8 +3,9 @@ apt-add-repository universe
 apt-get update
 apt-get install qt5-default pkg-config -y
 cd /usr/src
-wget http://developer.download.nvidia.com/embedded/L4T/r28_Release_v1.0/BSP/source_release.tbz2
-tar -xvf source_release.tbz2 sources/kernel_src-tx2.tbz2
+wget https://developer.download.nvidia.com/embedded/L4T/r32_Release_v1.0/jax-tx2/BSP/JAX-TX2-public_sources.tbz2
+tar -xvf JAX-TX2-public_sources.tbz2 sources/kernel_src-tx2.tbz2
+exit 0
 tar -xvf sources/kernel_src-tx2.tbz2
 cd kernel/kernel-4.4
 #zcat /proc/config.gz > .config
