1a45bb46d8a4594cedf431a54939401da276f3a0
Agustin Rivero <txtmode@gmail.com>
2019-05-16 14:11:20 +0200

    Fix modifications for 4.9 kernel

--- scripts/getKernelSources.sh
+++ scripts/getKernelSources.sh
@@ -4,10 +4,9 @@ apt-get update
 apt-get install qt5-default pkg-config -y
 cd /usr/src
 wget https://developer.download.nvidia.com/embedded/L4T/r32_Release_v1.0/jax-tx2/BSP/JAX-TX2-public_sources.tbz2
-tar -xvf JAX-TX2-public_sources.tbz2 sources/kernel_src-tx2.tbz2
-exit 0
-tar -xvf sources/kernel_src-tx2.tbz2
-cd kernel/kernel-4.4
+tar -xvf JAX-TX2-public_sources.tbz2 public_sources/kernel_src.tbz2
+tar -xvf public_sources/kernel_src.tbz2
+cd kernel/kernel-4.9
 #zcat /proc/config.gz > .config
 #patch -p0 < /home/nvidia/buildJetsonTX2Kernel/diffs/kernelconfig/config.patch
 #patch -p0 < /home/nvidia/buildJetsonTX2Kernel/diffs/kernelconfig/docker.patch
